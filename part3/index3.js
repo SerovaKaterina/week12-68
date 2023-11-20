98a9f61bbb60a685baa47b5a6123bf12c880900d
SerovaKaterina <k.serova.k@yandex.ru>
2023-11-20 22:56:12 +0200

    add changes to part3

--- part3/index3.js
+++ part3/index3.js
@@ -2,15 +2,24 @@ const btn = document.querySelector('.button');
 const more = document.getElementById('more');
 const result = document.getElementById('result');
 const info = document.querySelector('.info');
+let weightInput = document.getElementById('weight');
+let heightInput = document.getElementById('height');
+//Добавляем функцию проверки, что поля заполнены и если нет, отключаем кнопку
+function updateButtonState() {
+	btn.disabled = !weightInput.value || !heightInput.value;
+
+}
+updateButtonState();
+//Добавляем обработчики событий для апдейта состояния кнопки при изменении полей
+weightInput.addEventListener('input', updateButtonState);
+heightInput.addEventListener('input', updateButtonState);
 function calculateBMI () {
-const weight = document.getElementById('weight').value; //извлекаем значение введенное в инпут
-const height = document.getElementById('height').value; //извлекаем значение введенное в инпут
-let w = +weight; //преобразовываем строку в число
-let h = +height; //преобразовываем строку в число
-let hMetr = h /100; //приводим рост к метрам
-let bmi = w / Math.pow(hMetr, 2) ;  //находим коэффициент ИМТ
-result.textContent = bmi.toFixed(1);
-more.style.visibility = 'visible';
+    let w = +weightInput.value; //преобразовываем строку в число
+    let h = +heightInput.value; //преобразовываем строку в число
+    let hMetr = h /100; //приводим рост к метрам
+    let bmi = w / Math.pow(hMetr, 2) ;  //находим коэффициент ИМТ
+    result.textContent = bmi.toFixed(1);
+    more.style.visibility = 'visible';
 }
 btn.addEventListener('click', calculateBMI);
 
